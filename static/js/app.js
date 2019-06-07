f022b3fbb473e7454b29bd85781e909dc1abd26b
tpj728 <46807198+tpj728@users.noreply.github.com>
2019-06-07 15:44:46 -0700

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -8,60 +8,87 @@ submit.on("click", function() {
     // remove auto refreshing
     d3.event.preventDefault();
     // select the input element and get HTML id
-    let inputElement = d3.select("#datetime");
     // get the users input element
-    let inputValue = inputElement.property("value");
+    let inputE1 = d3.select("#datetime");
+    let inputV1 = inputE1.property("value");
 
-    // let inputElement1 = d3.select("#city");
-    // let inputValue1 = inputElement1.property("value");
+    let inputE2 = d3.select("#city");
+    let inputV2 = inputE2.property("value");
 
+    
+    let inputE3 = d3.select("#state");
+    let inputV3 = inputE3.property("value");
+    
+    let inputE4 = d3.select("#shape");
+    let inputV4 = inputE4.property("value");
 
     // test outputs
-    // console.log(inputValue);
+    console.log(inputV1);
+    console.log(inputV2);
+    console.log(inputV3);
+    console.log(inputV4);
+
+    let filteredData = tableData
+   
+    if (inputV1.length > 0 ) {
+        filteredData = filteredData.filter(tableData => tableData.datetime === inputV1);
+        } else {
+        filteredData = filteredData;
+    }
+
+    if (inputV2.length > 0 ) {
+        filteredData = filteredData.filter(tableData => tableData.city === inputV2);
+        } else {
+        filteredData = filteredData;
+    }
+
+    if (inputV3.length > 0 ) {
+        filteredData = filteredData.filter(tableData => tableData.state === inputV3);
+        } else {
+        filteredData = filteredData;
+    }
+
+    if (inputV4.length > 0 ) {
+        filteredData = filteredData.filter(tableData => tableData.shape === inputV4);
+        } else {
+        filteredData = filteredData;
+    }
+
+
     // console.log(tableData);
 
-    // Tag to rename label name - not needed.
-    // d3.select("li>label").text(inputValue);
-
-    let filteredData = tableData.filter(tableData => tableData.datetime === inputValue)
-
-//****/ || tableData.city === inputValue1****
-
-
-
-    console.log(filteredData);
 
     let tbody = d3.select("tbody");
 
-    // // // // Step 1: Loop Through `data` and console.log each data.js object
-// // filteredData.forEach(function(data) {
-// // //   console.log(data);
-// // });
+//     // // // // Step 1: Loop Through `data` and console.log each data.js object
+    filteredData.forEach(function(data) {
+      console.log(data);
+    });
 
-// // filteredData.forEach(data => console.log(data));
+// // // filteredData.forEach(data => console.log(data));
 
-// // Step 2:  Use d3 to append one table row `tr` for each data.js object
-// // Appending the `tr` elements.
+// // // Step 2:  Use d3 to append one table row `tr` for each data.js object
+// // // Appending the `tr` elements.
         filteredData.forEach(data => {
-    //   console.log(data);
+            console.log(data);
             let row = tbody.append("tr");
         });
     
-    // // // Step 3:  Use `Object.entries` to console.log each data.js value
+//     // // // Step 3:  Use `Object.entries` to console.log each data.js value
         filteredData.forEach(function(data) {
-    // //   console.log(data);
+            console.log(data);
             let row = tbody.append("tr");
             Object.entries(data).forEach(([key, value]) => console.log(key, value))
         });
     
-    // // // Step 4: Use d3 to append 1 cell per data.js value (Date, City, State, Country, Shape, Duration, Comments)
+//     // // // Step 4: Use d3 to append 1 cell per data.js value (Date, City, State, Country, Shape, Duration, Comments)
         filteredData.forEach(function(data){
-    //     // console.log(data);
+            console.log(data);
             let row = tbody.append("tr");
             Object.entries(data).forEach(function([key, value]) {
-    //     // console.log(key, value);
-    //     // Append a cell to the row for each value
-    //     // in the data.js object
+            console.log(key, value);
+        // Append a cell to the row for each value
+        // in the data.js object
             let cell = row.append("td");
             cell.text(value)
             });
